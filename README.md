e2c299a1d7ee84a02fc3206991bc9b8480444588
TheGreasyMuffin <lbuckland0223@gmail.com>
2020-08-19 11:43:10 +0000

    basic views and routes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Chat App
 
-This is a chat app, created by flask. The purpose of this app is to take data from the url and store the information in a link
+This is a chat app; created in flask. The purpose of this app is to take data from the url and store the information in a link
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import os 
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<p>Hello</p>"
+    """Main page with instructions"""
+    return "Want to send a message? /USERNAME/MESSAGE"
+
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
